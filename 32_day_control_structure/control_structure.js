8711d28370256c3275a4d946042fbcfcf25f6bd5
ShamalAmbalkar <ambalkarshamal02@gmail.com>
2025-06-09 14:56:24 +0530

    Day 32 - Control Structure

---
+++ 32_day_control_structure/control_structure.js
@@ -0,0 +1,82 @@
+//conditional statement
+if(6>12)
+{
+	console.log("Monday");
+}
+
+
+if(7>12)
+{
+	console.log("pavan");
+}
+else
+{
+	console.log("mahesh");
+
+}
+
+
+if(7>12)
+{
+	console.log("pavan");
+}
+else if(6>12)
+{
+	console.log("mahesh");
+
+}
+else if(6>2)
+{
+	console.log("prabhas");
+}
+else
+{
+	console.log("NTR");
+}
+
+
+if(7>12)
+{
+	console.log("pavan");
+}
+if(6>12)
+{
+	console.log("mahesh");
+}
+if(6>2)
+{
+	console.log("prabhas");
+}
+
+//looping statement
+for(var s=1; s<=5; s++){
+    console.log(s);
+}
+
+var r=1;
+while(r<=3){
+	console.log(r);
+	r++;
+}
+
+var t=1;
+do{
+	console.log(t);
+	t++;
+}while(t<=6);
+
+for(var j=1;j<=2;j++){
+	for(var k=1;k<=2;k++){
+		console.log(k);
+	}
+}
+
+//jumping statements
+for(var g=1;g<=10;g++){
+	if(g==6){
+		break;
+	}
+	else{
+		console.log(g);
+	}
+}
